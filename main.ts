54e94adbb8bbdd4ebde2e9b6098a1a6948ffd864
Andreas Jahn <110964118+akurtj@users.noreply.github.com>
2022-08-10 18:03:46 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -72,21 +72,6 @@ namespace FtCalliMicro {
         return val
     }
 
-/**
-    function setPwm(channel: number, on: number, off: number): void {
-        if (channel < 0 || channel > 15)
-            return;
-
-        let buf3 = pins.createBuffer(5);
-        buf3[0] = LED0_ON_L + 4 * channel;
-        buf3[1] = on & 0xff;
-        buf3[2] = (on >> 8) & 0xff;
-        buf3[3] = off & 0xff;
-        buf3[4] = (off >> 8) & 0xff;
-        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf3);
-    }
-*/
-
     /**
 	 * Servo control function.
      * S1~S4.
@@ -105,66 +90,4 @@ namespace FtCalliMicro {
         i2cWrite(FTCALLIMICRO_I2C_ADDRESS, index, value)
     }
 
-    /**
-	 * Execute a motor
-     * M1~M4.
-     * speed(0~255).
-    */
-    //% weight=90
-    //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed|%"
-    //% speed.min=0 speed.max=100
-    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
-    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
-    //% speed.shadow="speedPicker"
-    //% group="Motor"
-/**
-    export function MotorRun(index: Motors, direction: Dir, speed: number): void {
-        
-        speed = speed * 41 * direction; // map 100 to 4096 (naja 4100)
-        if (speed >= 4096) {
-            speed = 4095
-        }
-        if (speed <= -4096) {
-            speed = -4095
-        }
-        if (index > 4 || index <= 0)
-            return
-        let pn = (4 - index) * 2
-        let pp = (4 - index) * 2 + 1
-        if (speed >= 0) {
-            setPwm(pp, 0, speed)
-            setPwm(pn, 0, 0)
-        } else {
-            setPwm(pp, 0, 0)
-            setPwm(pn, 0, -speed)
-        }
-    }
-*/
-
-    /**
-	 * Stop the dc motor.
-    */
-    //% weight=85
-    //% blockId=motor_motorStop block="Motor stop|%index"
-    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2 
-    //% group="Motor"
-/**
-    export function motorStop(index: Motors) {
-        setPwm((4 - index) * 2, 0, 0);
-        setPwm((4 - index) * 2 + 1, 0, 0);
-    }
-*/
-    /**
-	 * Stop all motors
-    */
-    //% weight=84
-    //% blockId=motor_motorStopAll block="Motor Stop All"
-    //% group="Motor"
-/**
-    export function motorStopAll(): void {
-        for (let idx = 1; idx <= 4; idx++) {
-            motorStop(idx);
-        }
-    }
-*/
 }
